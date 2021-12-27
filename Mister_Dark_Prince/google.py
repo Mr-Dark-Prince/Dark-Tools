101d16d7d5e3cb2a29d599311b841557a14c507d
Mr Dark Prince <73339924+Mister-Dark-Prince@users.noreply.github.com>
2021-12-28 05:17:06 +0530

    Create google.py

---
+++ Mister_Dark_Prince/google.py
@@ -0,0 +1,25 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from config import HNDLR
+
+
+@Client.on_message(filters.command(["google", "g"], prefixes=f"{HNDLR}") & filters.me)
+async def webshot(client: Client, message: Message):
+    user_request = " ".join(message.command[1:])
+
+    if user_request == "":
+        if message.reply_to_message:
+            reply_user_request = message.reply_to_message.text
+            request = reply_user_request.replace(" ", "+")
+            full_request = f"https://lmgtfy.app/?s=g&iie=1&q={request}"
+            await message.edit(
+                f"<a href={full_request}>{reply_user_request}</a>",
+                disable_web_page_preview=True,
+            )
+
+    else:
+        request = user_request.replace(" ", "+")
+        full_request = f"https://lmgtfy.app/?s=g&iie=1&q={request}"
+        await message.edit(
+            f"<a href={full_request}>{user_request}</a>", disable_web_page_preview=True
+        )
