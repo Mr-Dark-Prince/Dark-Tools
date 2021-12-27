0fc09c08c9162107f342433837c5e4f0b9fce12a
Mr Dark Prince <73339924+Mister-Dark-Prince@users.noreply.github.com>
2021-12-27 22:59:59 +0530

    ഹിഹി 😁

--- main.py
+++ main.py
@@ -3,19 +3,17 @@ import asyncio
 from pytgcalls import idle
 
 from config import call_py
-from EvaMaria.quote import arq
 
 async def main():
     await call_py.start()
     print(
         """
     ------------------
-   | EvaMaria Started! |
+   | Dark Prince Started! |
     ------------------
 """
     )
     await idle()
-    await arq.close()
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
