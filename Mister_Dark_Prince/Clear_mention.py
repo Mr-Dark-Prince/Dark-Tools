fc7a1d2842d908bf8a3f070aeeb1b52826edb4e4
Mr Dark Prince <73339924+Mister-Dark-Prince@users.noreply.github.com>
2021-12-28 02:00:48 +0530

    Create Clear_mention.py

---
+++ Mister_Dark_Prince/Clear_mention.py
@@ -0,0 +1,33 @@
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+from pyrogram.raw import functions, types
+from pyrogram.types import Message
+from config import HNDLR
+
+@Client.on_message(filters.command(["clear@"], prefixes=f"{HNDLR}") & filters.me)
+async def solo_clear_handler(c: Client, m: Message):
+    await m.delete()
+    peer = await c.resolve_peer(m.chat.id)
+    request = functions.messages.ReadMentions(peer=peer)
+    await c.send(request)
+
+
+@Client.on_message(filters.command(["all@"], prefixes=f"{HNDLR}") & filters.me)
+async def global_clear_handler(c: Client, m: Message):
+    request = functions.messages.GetAllChats(except_ids=[])
+    try:
+        result = await c.send(request)
+    except FloodWait as e:
+        await m.edit_text(
+            f"<code>FloodWait received. Wait {e.x} seconds before trying again</code>"
+        )
+        return
+    await m.delete()
+    for chat in result.chats:
+        if type(chat) is types.Chat:
+            peer_id = -chat.id
+        elif type(chat) is types.Channel:
+            peer_id = int(f"-100{chat.id}")
+        peer = await c.resolve_peer(peer_id)
+        request = functions.messages.ReadMentions(peer=peer)
+        await c.send(request)
