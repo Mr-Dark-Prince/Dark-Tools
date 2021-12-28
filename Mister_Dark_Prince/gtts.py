52420db4f8dcf37a94e750e76183142f7e37e803
Mr Dark Prince <73339924+Mister-Dark-Prince@users.noreply.github.com>
2021-12-28 12:59:15 +0530

    Create gtts.py

---
+++ Mister_Dark_Prince/gtts.py
@@ -0,0 +1,27 @@
+from io import BytesIO
+
+from gtts import gTTS
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from config import HNDLR
+
+
+@Client.on_message(filters.command("tts", prefixes=f"{HNDLR}") & filters.me)
+async def tts(client: Client, message: Message):
+    lang = message.command[1]
+    text = " ".join(message.command[2:])
+    await message.edit("<code>Speech synthesis...</code>")
+    tts = gTTS(text, lang=lang)
+    voice = BytesIO()
+    tts.write_to_fp(voice)
+    voice.name = "voice.ogg"
+    await message.delete()
+    if message.reply_to_message:
+        await client.send_audio(
+            message.chat.id,
+            voice,
+            reply_to_message_id=message.reply_to_message.message_id,
+        )
+    else:
+        await client.send_audio(message.chat.id, voice)
