9b779e79b15e2b754d176919def4190690c2b6ff
Mr Dark Prince <73339924+Mister-Dark-Prince@users.noreply.github.com>
2021-12-28 16:12:08 +0530

    Update OFFLINE.py

--- Mister_Dark_Prince/OFFLINE.py
+++ Mister_Dark_Prince/OFFLINE.py
@@ -1,9 +1,9 @@
-from main import app
+from pyrogram import Client
 import pyrogram
 
 
-@app.on_user_status()
-async def make_offline(_, user):
+@Client.on_user_status()
+async def make_offline(_, user client: Client):
     if user.id == 5029694040 and user.status == "online":
-        await app.send(pyrogram.raw.functions.account.UpdateStatus(offline=True))
+        await client.send(pyrogram.raw.functions.account.UpdateStatus(offline=True))
         print(m.from_user.status)
