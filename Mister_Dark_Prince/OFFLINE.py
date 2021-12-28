6cf6ffca4adb3a18700a1f1e633a6604ed052c72
Mr Dark Prince <73339924+Mister-Dark-Prince@users.noreply.github.com>
2021-12-28 16:17:34 +0530

    Delete OFFLINE.py

--- Mister_Dark_Prince/OFFLINE.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client
-import pyrogram
-
-
-@Client.on_user_status()
-async def make_offline(_, user client: Client):
-    if user.id == 5029694040 and user.status == "online":
-        await client.send(pyrogram.raw.functions.account.UpdateStatus(offline=True))
-        print(m.from_user.status)
