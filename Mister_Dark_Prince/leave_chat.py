3922cac40e7779d8f23a4b53e5f35b98a057e401
Mr Dark Prince <73339924+Mister-Dark-Prince@users.noreply.github.com>
2021-12-28 11:53:05 +0530

    Create leave_chat.py

---
+++ Mister_Dark_Prince/leave_chat.py
@@ -0,0 +1,16 @@
+import asyncio
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from config import HNDLR
+
+
+@Client.on_message(filters.command(["leave_chat", "lc"], prefixes=f"{HNDLR}") & filters.me)
+async def leave_chat(client: Client, message: Message):
+    if message.chat.type in ["group", "supergroup"]:
+        await message.edit("<code>Goodbye...</code>")
+        await asyncio.sleep(3)
+        await client.leave_chat(chat_id=message.chat.id)
+    else:
+        await message.edit("This is not a group/suppergroup")
