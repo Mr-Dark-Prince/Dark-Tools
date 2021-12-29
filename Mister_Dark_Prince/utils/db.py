714951a3568bff5bee1ce8f49a68d8fe04823975
Mr Dark Prince <73339924+Mister-Dark-Prince@users.noreply.github.com>
2021-12-29 22:07:10 +0530

    Create db.py

---
+++ Mister_Dark_Prince/utils/db.py
@@ -0,0 +1,62 @@
+import configparser
+import os
+import sys
+
+import dns.resolver
+import pymongo as md
+
+dns.resolver.default_resolver = dns.resolver.Resolver(configure=False)
+dns.resolver.default_resolver.nameservers = ["8.8.8.8"]
+
+
+class DataBase:
+    def __init__(self):
+        config_path = os.path.join(sys.path[0], "config.py")
+        config = configparser.ConfigParser()
+        config.read(config_path)
+        db_url = config.get("pyrogram", "db_url")
+        try:
+            db_name = config.get("db", "db_name")
+        except:
+            try:
+                config.add_section("db")
+            except:
+                pass
+            config.set("db", "db_name", "Dark_Tools")
+            with open(config_path, "w") as f:
+                config.write(f)
+            os.system("python3 main.py")
+        self._DB = md.MongoClient(db_url)[db_name]
+
+    def set(self, module: str, variable: str, value):
+        modcollection = self._DB[module]
+        doc = modcollection.find_one({"var": variable})
+        if doc is None:
+            modcollection.insert_one({"var": variable, "val": value})
+        else:
+            modcollection.replace_one(doc, {"var": variable, "val": value})
+        return True
+
+    def get(self, module: str, variable: str, expected_value=None):
+        modcollection = self._DB[module]
+        doc = modcollection.find_one({"var": variable})
+        if doc is None:
+            return expected_value
+        else:
+            return doc["val"]
+
+    def get_collection(self, module: str):
+        modcollection = self._DB[module]
+        return [{_["var"]: _["val"]} for _ in modcollection.find()]
+
+    def remove(self, module: str, variable: str):
+        modcollection = self._DB[module]
+        doc = modcollection.find_one({"var": variable})
+        if doc != None:
+            modcollection.delete_one(doc)
+            return True
+        else:
+            return False
+
+
+db = DataBase()
