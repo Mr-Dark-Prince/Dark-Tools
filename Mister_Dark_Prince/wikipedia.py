b1cbf577a1b93caf275d81a87d7b1de8b498c207
Mr Dark Prince <73339924+Mister-Dark-Prince@users.noreply.github.com>
2021-12-30 04:44:29 +0530

    Create wikipedia.py

---
+++ Mister_Dark_Prince/wikipedia.py
@@ -0,0 +1,41 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+import wikipedia
+from .darkprince.helpo import modules_help
+from .darkprince.helpo import requirements_list
+
+
+@Client.on_message(filters.command("wiki", prefixes=f"{HNDLR}") & filters.me)
+async def wiki(client: Client, message: Message):
+    lang = message.command[1]
+    user_request = " ".join(message.command[2:])
+    if user_request == "":
+        wikipedia.set_lang("en")
+        user_request = " ".join(message.command[1:])
+    try:
+        if lang == "ml":
+            wikipedia.set_lang("ml")
+
+        result = wikipedia.summary(user_request)
+        await message.edit(
+            f"""<b>Request:</b>
+<code>{user_request}</code>
+<b>Result:</b>
+<code>{result}</code>"""
+        )
+
+    except Exception as exc:
+        await message.edit(
+            f"""<b>Request:</b>
+<code>{user_request}</code>
+<b>Result:</b>
+<code>{exc}</code>"""
+        )
+
+
+modules_help.append(
+    {"wikipedia": [{"wiki [lang] [request]*": "Search in Russian Wikipedia"}]}
+)
+
+requirements_list.append("wikipedia")
