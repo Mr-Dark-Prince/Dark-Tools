9be4591268ecaa5a4c876534069e95b8676071c3
Mr Dark Prince <73339924+Mister-Dark-Prince@users.noreply.github.com>
2021-12-30 04:53:03 +0530

    Create tiktok.py

---
+++ Mister_Dark_Prince/tiktok.py
@@ -0,0 +1,36 @@
+import asyncio
+
+from pyrogram import Client, filters
+from pyrogram.raw import functions
+from pyrogram.types import Message
+from .darkprince.helpo import modules_help, prefix
+from config import HNDLR 
+
+@Client.on_message(filters.command("tt", prefixes="{HNDLR}") & filters.me)
+async def tiktok(client: Client, message: Message):
+    if message.reply_to_message:
+        link = message.reply_to_message.text
+    elif len(message.command) == 2:
+        link = message.command[1]
+    else:
+        return await message.edit(
+            "<i>You have not provided a link, check out the documentation for this module</i>"
+        )
+    await message.edit("<i> Loading...</i>")
+    await client.send_message("@ttlessbot", "/start")
+    await asyncio.sleep(0.5)
+    await client.send_message("@ttlessbot", link)
+    await asyncio.sleep(7)
+    messages = await client.get_history("@ttlessbot")
+    video = messages[1].video.file_id
+    await message.delete()
+    await client.send_video(message.chat.id, video)
+
+
+modules_help.append(
+    {
+        "tiktok": [
+            {"tt [link]/[reply]*": " Download video from TikTok and send it to chat "}
+        ]
+    }
+)
