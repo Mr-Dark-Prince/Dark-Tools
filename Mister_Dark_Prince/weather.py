d1163bd43171e4b45cd7ae12bc2fd0ffe8f2eb4b
Mr Dark Prince <73339924+Mister-Dark-Prince@users.noreply.github.com>
2021-12-29 15:26:26 +0530

    Create weather.py

---
+++ Mister_Dark_Prince/weather.py
@@ -0,0 +1,43 @@
+import asyncio
+import os
+
+import requests
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from config import HNDLR
+
+
+def get_pic(city):
+    file_name = f"{city}.png"
+    with open(file_name, "wb") as pic:
+        response = requests.get(f"http://wttr.in/{city}_2&lang=en.png", stream=True)
+
+        if not response.ok:
+            print(response)
+
+        for block in response.iter_content(1024):
+            if not block:
+                break
+
+            pic.write(block)
+        return file_name
+
+
+@Client.on_message(filters.command("weather", prefixes=f"{HNDLR}") & filters.me)
+async def weather(client: Client, message: Message):
+    try:
+        city = message.command[1]
+        await message.edit("```Processing the request...```")
+        r = requests.get(f"https://wttr.in/{city}?m?M?0?q?T&lang=en")
+        await message.edit(f"```City: {r.text}```")
+        await client.send_document(
+            chat_id=message.chat.id,
+            document=get_pic(city),
+            reply_to_message_id=message.message_id,
+        )
+        os.remove(f"{city}.png")
+    except:
+        await message.edit("<code>Error occured</code>")
+        await asyncio.sleep(5)
+        await message.delete()
