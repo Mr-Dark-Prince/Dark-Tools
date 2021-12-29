f4fab6a8596ab3725352c3ccb6cfe16da15a5fdf
Mr Dark Prince <73339924+Mister-Dark-Prince@users.noreply.github.com>
2021-12-29 21:00:11 +0530

    Create afk.py

---
+++ Mister_Dark_Prince/afk.py
@@ -0,0 +1,53 @@
+import asyncio
+import datetime
+
+from pyrogram import Client, filters
+from pyrogram.handlers import MessageHandler
+
+from .darkprince.helpo import modules_help
+from config import HNDLR
+
+async def afk_handler(client, message):
+    try:
+        global start, end
+        end = datetime.datetime.now().replace(microsecond=0)
+        afk_time = end - start
+        if message.from_user.is_bot is False:
+            await message.reply_text(
+                f"<b>I afk {afk_time}</b>\n" f"<b>Reason:</b> <i>{reason}</i>"
+            )
+    except NameError:
+        pass
+
+
+@Client.on_message(filters.command("afk", prefixes=f"{HNDLR}") & filters.me)
+async def afk(client, message):
+    global start, end, handler, reason
+    start = datetime.datetime.now().replace(microsecond=0)
+    handler = client.add_handler(
+        MessageHandler(afk_handler, (filters.private & ~filters.me))
+    )
+    if len(message.text.split()) >= 2:
+        reason = message.text.split(" ", maxsplit=1)[1]
+    else:
+        reason = "None"
+    await message.edit("<b>I'm going afk</b>")
+
+
+@Client.on_message(filters.command("unafk", prefixes=f"{HNDLR}") & filters.me)
+async def unafk(client, message):
+    try:
+        global start, end
+        end = datetime.datetime.now().replace(microsecond=0)
+        afk_time = end - start
+        await message.edit(f"<b>I'm not AFK anymore.\nI was afk {afk_time}</b>")
+        client.remove_handler(*handler)
+    except NameError:
+        await message.edit("<b>You weren't afk</b>")
+        await asyncio.sleep(3)
+        await message.delete()
+
+
+modules_help.append(
+    {"afk": [{"afk [reason]": "Go to afk"}, {"unafk": "Get out of AFK"}]}
+)
