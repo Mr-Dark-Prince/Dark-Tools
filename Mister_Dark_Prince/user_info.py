e9a2d53d9938c75f156086f5f4bec0d73a908630
Mr Dark Prince <73339924+Mister-Dark-Prince@users.noreply.github.com>
2021-12-30 00:47:43 +0530

    Create user_info.py

---
+++ Mister_Dark_Prince/user_info.py
@@ -0,0 +1,117 @@
+import asyncio
+
+from pyrogram import Client, filters
+from pyrogram.raw import functions
+from pyrogram.types import Message
+
+from .darkprince.scripts import date_dict
+from .darkprince.helpo import modules_help
+from config import HNDLR
+
+
+@Client.on_message(filters.command("inf", prefixes=f"{HNDLR}") & filters.me)
+async def get_user_inf(client: Client, message: Message):
+    if len(message.text.split()) >= 2:
+        try:
+            user = await client.get_users(message.text.split()[1])
+            user = user.id
+        except:
+            try:
+                user = message.reply_to_message.from_user.id
+            except:
+                user = message.from_user.id
+    else:
+        try:
+            user = message.reply_to_message.from_user.id
+        except:
+            user = message.from_user.id
+    user_info = await client.send(
+        functions.users.GetFullUser(id=await client.resolve_peer(user))
+    )
+    if user_info.user.username is None:
+        username = "None"
+    else:
+        username = f"@{user_info.user.username}"
+    about = "None" if user_info.about is None else user_info.about
+    user_info = f"""|=<b>Username: {username}
+|-Id: <code>{user_info.user.id}</code>
+|-Bot: <code>{user_info.user.bot}</code>
+|-Scam: <code>{user_info.user.scam}</code>
+|-Name: <code>{user_info.user.first_name}</code>
+|-Deleted: <code>{user_info.user.deleted}</code>
+|-BIO: <code>{about}</code>
+</b>"""
+    await message.edit(user_info)
+
+
+@Client.on_message(filters.command("inffull", prefixes=f"{HNDLR}") & filters.me)
+async def get_full_user_inf(client: Client, message: Message):
+    await message.edit("<code>Receiving the information...</code>")
+    if len(message.text.split()) >= 2:
+        try:
+            user = await client.get_users(message.text.split()[1])
+            user = user.id
+        except:
+            try:
+                user = message.reply_to_message.from_user.id
+            except:
+                user = message.from_user.id
+    else:
+        try:
+            user = message.reply_to_message.from_user.id
+        except:
+            user = message.from_user.id
+    try:
+        await client.send_message("@creationdatebot", '/start')
+        await asyncio.sleep(1)
+        date_dict.clear()
+        msg = await client.send_message("@creationdatebot", f"/id {user}")
+        await asyncio.sleep(1)
+        await client.send(
+            functions.messages.DeleteHistory(
+                peer=await client.resolve_peer(747653812), max_id=msg.chat.id
+            )
+        )
+        user_info = await client.send(
+            functions.users.GetFullUser(id=await client.resolve_peer(user))
+        )
+        if user_info.user.username is None:
+            username = "None"
+        else:
+            username = f"@{user_info.user.username}"
+        about = "None" if user_info.about is None else user_info.about
+        user_info = f"""|=<b>Username: {username}
+|-Id: <code>{user_info.user.id}</code>
+|-Account creation date: <code>{date_dict.get('date')}</code>
+|-Bot: <code>{user_info.user.bot}</code>
+|-Scam: <code>{user_info.user.scam}</code>
+|-Name: <code>{user_info.user.first_name}</code>
+|-Deleted: <code>{user_info.user.deleted}</code>
+|-BIO: <code>{about}</code>
+|-Contact: <code>{user_info.user.contact}</code>
+|-Can pin message: <code>{user_info.can_pin_message}</code>
+|-Mutual contact: <code>{user_info.user.mutual_contact}</code>
+|-Access hash: <code>{user_info.user.access_hash}</code>
+|-Restricted: <code>{user_info.user.restricted}</code>
+|-Verified: <code>{user_info.user.verified}</code>
+|-Phone calls available: <code>{user_info.phone_calls_available}</code>
+|-Phone calls private: <code>{user_info.phone_calls_private}</code>
+|-Blocked: <code>{user_info.blocked}</code></b>"""
+        date_dict.clear()
+        await message.edit(user_info)
+    except:
+        await message.edit("<code>An error has occurred...</code>")
+
+
+modules_help.append(
+    {
+        "user_info": [
+            {
+                "inf [reply]/[user id]*": "Reply to any user message to find out brief information about him"
+            },
+            {
+                "inffull [reply]/[user id]*": "Reply to any user message to find out full information about him"
+            },
+        ]
+    }
+)
