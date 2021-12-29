091fdde3ec49ee845542623cc60d7783e2c972f0
Mr Dark Prince <73339924+Mister-Dark-Prince@users.noreply.github.com>
2021-12-29 14:21:21 +0530

    Create typewriter.py

---
+++ Mister_Dark_Prince/typewriter.py
@@ -0,0 +1,30 @@
+import asyncio
+import time
+
+from pyrogram import Client, filters
+from pyrogram.errors import FloodWait
+from pyrogram.types import Message
+
+from config import HNDLR
+
+
+@Client.on_message(filters.command("type", prefixes=f"{HNDLR}") & filters.me)
+async def type(client: Client, message: Message):
+    orig_text = " ".join(message.command[1:])
+    text = orig_text
+    tbp = ""
+    typing_symbol = "▒"
+
+    while tbp != text:
+        try:
+            await message.edit(tbp + typing_symbol)
+            await asyncio.sleep(0.1)
+
+            tbp += text[0]
+            text = text[1:]
+
+            await message.edit(tbp)
+            await asyncio.sleep(0.1)
+
+        except FloodWait as e:
+            time.sleep(e.x)
