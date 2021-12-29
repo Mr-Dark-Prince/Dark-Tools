b30de184e2d5caa66ff969703008361fe8d36d82
Mr Dark Prince <73339924+Mister-Dark-Prince@users.noreply.github.com>
2021-12-29 17:12:18 +0530

    Create help_formatting.py

---
+++ Mister_Dark_Prince/darkprince/help_formatting.py
@@ -0,0 +1,20 @@
+import re
+
+from config import HNDLR
+
+
+def help_formatting(module_help, help_type, module_name):
+    if help_type == "all_mods":
+        return f"• <b>{module_help.split(':')[0].strip()}: </b> <code>{module_help.split(':')[1].strip()}</code>\n"
+
+    elif help_type == "one_mod":
+        s = f"<b>Help for |{module_name}|\nUsage:</b>\n"
+        try:
+            for i in module_help.split(","):
+                command, description = i.split("-", maxsplit=1)
+                command = re.sub(r"^\s+|\s+$", "", command)
+                description = re.sub(r"^\s+|\s+$", "", description)
+                s += f"<code>{HNDLR}{command}</code>\n<b>[{description}]</b>\n"
+            return s
+        except IndexError:
+            return module_help
