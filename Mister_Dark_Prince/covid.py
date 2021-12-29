074683a639b29fe357adc95e8c06a2abd62b3841
Mr Dark Prince <73339924+Mister-Dark-Prince@users.noreply.github.com>
2021-12-29 20:49:36 +0530

    Create covid.py

---
+++ Mister_Dark_Prince/covid.py
@@ -0,0 +1,57 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from covid import Covid
+from .darkprince.helpo import modules_help
+from .darkprince.helpo import requirements_list
+
+from config import HNDLR
+
+@Client.on_message(filters.command("covid", prefixes=f"{HNDLR}") & filters.me)
+async def covid_local(client: Client, message: Message):
+    region = " ".join(message.command[1:])
+    await message.edit("<code>Data retrieval...</code>")
+    covid = Covid(source="worldometers")
+    try:
+        local_status = covid.get_status_by_country_name(region)
+        await message.edit(
+            "<b>=======🦠 COVID-19 STATUS 🦠=======</b>\n"
+            + f"<b>Region</b>: <code>{local_status['country']}</code>\n"
+            + "<b>====================================</b>\n"
+            + f"<b>🤧 New cases</b>: <code>{local_status['new_cases']}</code>\n"
+            + f"<b>😷 New deaths</b>: <code>{local_status['new_deaths']}</code>\n"
+            + "<b>====================================</b>\n"
+            + f"<b>😷 Сonfirmed</b>: <code>{local_status['confirmed']}</code>\n"
+            + f"<b>❗️ Active:</b> <code>{local_status['active']}</code>\n"
+            + f"<b>⚠️ Critical</b>: <code>{local_status['critical']}</code>\n"
+            + f"<b>💀 Deaths</b>: <code>{local_status['deaths']}</code>\n"
+            + f"<b>🚑 Recovered</b>: <code>{local_status['recovered']}</code>\n"
+        )
+    except ValueError:
+        await message.edit(f'<code>There is no region called "{region}"</code>')
+
+
+@Client.on_message(filters.command("regions", prefixes=f"{HNDLR}") & filters.me)
+async def regions(client: Client, message: Message):
+    countr = ""
+    await message.edit("<code>Data retrieval...</code>")
+    covid = Covid(source="worldometers")
+    regions = covid.list_countries()
+    for region in regions:
+        region = f"{region}\n"
+        countr += region
+    await message.edit(f"<code>{countr}</code>")
+
+
+modules_help.append(
+    {
+        "covid": [
+            {"covid [region]*": "Status by region"},
+            {
+                "regions": "Available regions]\n=======================\n[Worldometer.info statistics are used"
+            },
+        ]
+    }
+)
+
+requirements_list.append("covid")
