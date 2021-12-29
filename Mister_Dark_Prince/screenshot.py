9ab70f50077669d0cafec5ed17147519b3a0a7ed
Mr Dark Prince <73339924+Mister-Dark-Prince@users.noreply.github.com>
2021-12-30 00:10:41 +0530

    Create screenshot.py

---
+++ Mister_Dark_Prince/screenshot.py
@@ -0,0 +1,35 @@
+import asyncio
+
+from pyrogram import Client, filters
+from pyrogram.raw import functions
+from pyrogram.types import Message
+
+from .darkprince.helpo import modules_help
+from config import HNDLR
+
+@Client.on_message(
+    filters.command(["scr", "screenshot"], prefixes=f"{HNDLR}") & filters.me
+)
+async def screenshot(client: Client, message: Message):
+    quantity = int(message.command[1])
+    await message.delete()
+    for _ in range(quantity):
+        await asyncio.sleep(0.1)
+        await client.send(
+            functions.messages.SendScreenshotNotification(
+                peer=await client.resolve_peer(message.chat.id),
+                reply_to_msg_id=0,
+                random_id=client.rnd_id(),
+            )
+        )
+
+
+modules_help.append(
+    {
+        "screenshot": [
+            {
+                "scr [amount of screenshots]": "Take a screenshot\nThis only works in private messages!"
+            }
+        ]
+    }
+)
