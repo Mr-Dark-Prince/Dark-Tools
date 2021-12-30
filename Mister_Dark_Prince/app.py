e663fa6514878bf1334017a3be11fff1c4fc55c9
Mr-Dark-Prince <73339924+Mr-Dark-Prince@users.noreply.github.com>
2021-12-30 04:53:54 +0000

    `Dark Prince`

--- Mister_Dark_Prince/app.py
+++ Mister_Dark_Prince/app.py
@@ -1,33 +1,51 @@
 import bs4
-import aiohttp
 import requests
-
-from config import bot
 from pyrogram.types import Message
-from config import HNDLR
+
+from config import HNDLR, bot
 
 
 @bot.on_message(filters.command("afk", prefixes=f"{HNDLR}") & filters.me)
 async def app(message: Message):
     try:
         await message.edit("`Searching...`")
-        app_name = '+'.join(message.input_str.split(' '))
+        app_name = "+".join(message.input_str.split(" "))
         page = requests.get(f"https://play.google.com/store/search?q={app_name}&c=apps")
-        soup = bs4.BeautifulSoup(page.content, 'lxml', from_encoding='utf-8')
+        soup = bs4.BeautifulSoup(page.content, "lxml", from_encoding="utf-8")
         results = soup.findAll("div", "ZmHEEd")
 
-        app_name = results[0].findNext('div', 'Vpfmgd').findNext('div', 'WsMG1c nnK0zc').text
-        app_dev = results[0].findNext('div', 'Vpfmgd').findNext('div', 'KoLSrc').text
-        app_dev_link = "https://play.google.com" + results[0].findNext(
-            'div', 'Vpfmgd').findNext('a', 'mnKHRc')['href']
-        app_rating = results[0].findNext('div', 'Vpfmgd').findNext(
-            'div', 'pf5lIe'
-        ).find('div')['aria-label'].replace("Rated ", "⭐️ ").replace(
-            " out of ", "/"
-        ).replace(" stars", "", 1).replace(" stars", "⭐️").replace("five", "5")
-        app_link = "https://play.google.com" + results[0].findNext(
-            'div', 'Vpfmgd').findNext('div', 'vU6FJ p63iDd').a['href']
-        app_icon = results[0].findNext('div', 'Vpfmgd').findNext('div', 'uzcko').img['data-src']
+        app_name = (
+            results[0].findNext("div", "Vpfmgd").findNext("div", "WsMG1c nnK0zc").text
+        )
+        app_dev = results[0].findNext("div", "Vpfmgd").findNext("div", "KoLSrc").text
+        app_dev_link = (
+            "https://play.google.com"
+            + results[0].findNext("div", "Vpfmgd").findNext("a", "mnKHRc")["href"]
+        )
+        app_rating = (
+            results[0]
+            .findNext("div", "Vpfmgd")
+            .findNext("div", "pf5lIe")
+            .find("div")["aria-label"]
+            .replace("Rated ", "⭐️ ")
+            .replace(" out of ", "/")
+            .replace(" stars", "", 1)
+            .replace(" stars", "⭐️")
+            .replace("five", "5")
+        )
+        app_link = (
+            "https://play.google.com"
+            + results[0]
+            .findNext("div", "Vpfmgd")
+            .findNext("div", "vU6FJ p63iDd")
+            .a["href"]
+        )
+        app_icon = (
+            results[0]
+            .findNext("div", "Vpfmgd")
+            .findNext("div", "uzcko")
+            .img["data-src"]
+        )
 
         app_details = f"[📲]({app_icon}) **{app_name}**\n\n"
         app_details += f"`Developer :` [{app_dev}]({app_dev_link})\n"
